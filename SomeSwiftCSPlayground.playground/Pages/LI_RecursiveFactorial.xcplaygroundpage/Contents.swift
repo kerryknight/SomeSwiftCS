382460b5e0a73428dc884a916509e1cb50bcd880
Kerry Knight <kerry@kerryknight.io>
2018-04-01 10:47:54 -0400

    Added 2nd factorial method

--- SomeSwiftCSPlayground.playground/Pages/LI_RecursiveFactorial.xcplaygroundpage/Contents.swift
+++ SomeSwiftCSPlayground.playground/Pages/LI_RecursiveFactorial.xcplaygroundpage/Contents.swift
@@ -2,6 +2,7 @@
 
 import Foundation
 
+// Method #1
 func factorial(_ n: Int, _ result: Int) -> Int {
     guard n > 0 else { return result }
     return factorial(n - 1, n * result)
@@ -11,6 +12,14 @@ func factorial(_ n: Int) -> Int {
     return factorial(n, 1)
 }
 
-let result = factorial(4)
-print(result) // 24
+print(factorial(4)) // 24
+
+// Method #2
+func factorial(of number: Int) -> Int {
+    return (1...number).reduce(1) { result, number in
+        return result * number
+    }
+}
+
+print(factorial(of: 4))
 //: [Next](@next)
